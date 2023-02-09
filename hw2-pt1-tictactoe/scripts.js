bd2479fcd2a907cc6132f744d860000e70c6e381
Gregory Lustiber <glustiber@gmail.com>
2023-02-09 16:58:35 -0500

    removed grid

--- hw2-pt1-tictactoe/scripts.js
+++ hw2-pt1-tictactoe/scripts.js
@@ -1,12 +1,8 @@
 $(document).ready(function(){
 
-    for (let i = 0; i < 9; i++) {
-        $(".board").append( $("<div class='square'></div>") );
-    }
-
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
-            $(".board2").append( $("<div class='square2'></div>") );
+            $(".board").append( $("<div class='square'></div>") );
         }
     }
 
