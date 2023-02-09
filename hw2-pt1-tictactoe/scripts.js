4a40b7de79d7ae6abe764466e63b23d4d1de48b9
Gregory Lustiber <glustiber@gmail.com>
2023-02-09 17:54:05 -0500

    Added turn display

--- hw2-pt1-tictactoe/scripts.js
+++ hw2-pt1-tictactoe/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function(){
 
+    let turn = "O";
+
+    $('.turn').html(turn);
+
     for (let i = 0; i < 3; i++) {
         for (let j = 0; j < 3; j++) {
             $(".board").append( $("<div class='square'></div>") );
