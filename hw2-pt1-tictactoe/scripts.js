c2d5d99a3a8930d72c9f96b80caf8695dac24fdc
Gregory Lustiber <glustiber@gmail.com>
2023-02-09 15:50:25 -0500

    added flex box layout

--- hw2-pt1-tictactoe/scripts.js
+++ hw2-pt1-tictactoe/scripts.js
@@ -3,5 +3,11 @@ $(document).ready(function(){
     for (let i = 0; i < 9; i++) {
         $(".board").append( $("<div class='square'></div>") );
     }
-    
+
+    for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+            $(".board2").append( $("<div class='square2'></div>") );
+        }
+    }
+
 });
